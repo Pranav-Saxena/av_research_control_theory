8b5e06c02e63dfbe8b9191d947f9994a60489e1d
Pranav-Saxena <pranavsaxena2003@gmail.com>
2024-01-17 00:05:29 +0530

    WASD Controller

---
+++ controllers/move_controller/move_controller.py
@@ -0,0 +1,42 @@
+from vehicle import Driver
+from controller import Keyboard
+driver = Driver()
+keyboard = Keyboard()
+
+while driver.step() != -1:
+    key = keyboard.getKey()
+    if(key == ord('W')):
+        driver.setCruisingSpeed(5)
+        driver.setBrakeIntensity(0)
+        driver.setSteeringAngle(0)
+        driver.setGear(1)
+        if driver.getCurrentSpeed()>10:
+            driver.setThrottle(0.2)
+        elif driver.getCurrentSpeed()>30:
+            driver.setThrottle(0)
+        else:
+            driver.setThrottle(0.4)
+        
+        
+    elif(key == ord('S')):
+        driver.setBrakeIntensity(0)
+        driver.setSteeringAngle(0)
+        driver.setCruisingSpeed(-5)
+        driver.setGear(-1)
+        if driver.getCurrentSpeed()>10:
+            driver.setThrottle(0.2)
+        elif driver.getCurrentSpeed()>30:
+            driver.setThrottle(0)
+        else:
+            driver.setThrottle(0.4)
+    elif(key == ord('X')):
+        driver.setThrottle(0)
+        driver.setBrakeIntensity(1)
+    elif(key == ord('A')):
+        print(driver.getSteeringAngle())
+        driver.setSteeringAngle(-0.15)
+ 
+    elif(key == ord('D')):
+        driver.setSteeringAngle(0.15)
+    else:
+        pass
